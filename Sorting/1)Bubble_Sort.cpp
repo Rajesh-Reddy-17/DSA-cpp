d5bcf71dc72cf0948d93816b6d71cb6d7dbd8f56
Rajesh-Reddy-17 <padalarajeshreddy99@gmail.com>
2026-07-18 10:32:25 +0530

    Update 1)Bubble_Sort.cpp

--- Sorting/1)Bubble_Sort.cpp
+++ Sorting/1)Bubble_Sort.cpp
@@ -1,1 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+void bubbleSort(int arr[] , int n) {
+    for(int i=0;i<n-1;i++) {
+        for(int j=0;j<n-i-1;j++) {
+            if(arr[j]>arr[j+1]) {
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+
+void printArray(int arr[] , int n) {
+    for(int i=0;i<n;i++) {
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main() {
+    int n=9;
+    int arr[] = {9,4,7,2,8,1,5,3,6};
+
+    bubbleSort(arr,n);
+    printArray(arr,n);
+
+    return 0;
+}
+
+
+/* Output :-
+1 2 3 4 5 6 7 8 9
+*/
